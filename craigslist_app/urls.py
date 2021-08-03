200455885524259f7a988f0844655cc921bcebac
jung341416 <jung341416@gmail.com>
2021-08-03 14:32:00 -0400

    assessment 4 retake

--- craigslist_app/urls.py
+++ craigslist_app/urls.py
@@ -5,7 +5,11 @@ urlpatterns = [
     path('new', views.category_new, name="category_new"),
     path('<int:category_id>', views.category_detail, name="category_detail"),
     path('<int:category_id>/edit', views.category_edit, name="category_edit"),
+    path('<int:category_id>/delete', views.category_delete, name="category_delete"),
     path('<int:category_id>/posts', views.post_list, name="post_list"),
+    path('<int:category_id>/posts/new', views.post_new, name="post_new"),
     path('<int:category_id>/posts/<int:post_id>', views.post_detail, name="post_detail"),
+    path('<int:category_id>/posts/<int:post_id>/edit', views.post_edit, name="post_edit"),
+    path('<int:category_id>/posts/<int:post_id>/delete', views.post_delete, name="post_delete")
    
 ]
--- craigslist_app/views.py
+++ craigslist_app/views.py
@@ -6,31 +6,43 @@ from craigslist_app.forms import CategoryForm, PostForm
 # Create your views here.
 def index(request):
     data = {
-        "all_categories": Category.objects.all()
+        "all_categories": get_all_categories()
     }
     return render(request, "pages/category/category_list.html", data)
+
 def category_new(request):
     form = CategoryForm(request.POST or None)
     data = {
+        "add_or_edit": "Add",
+        "form": form
+    }
+    if request.method == "POST":
+        try: 
+            form.save()
+            return redirect("home")
+        except:
+            return HttpResponse('Error during category creation')
+    return render(request, "pages/category/category_new.html", data)
+
+def category_edit(request, category_id):
+  
+    category = get_category(category_id)
+    form = CategoryForm(request.POST or None, instance=category)
+    data = {
+        "add_or_edit": 'Edit',
         "form": form
     }
     if request.method == "POST":
         try:
-             form.save()
-             return redirect("")
+            form.save()
+            return redirect("category_detail", category.id)
         except:
-            return HttpResponse('Error during category creation')
-    return render(request, "pages/category/category_new.html", data)
-def category_edit(request, category_id):
-    form = CategoryForm()
-    data = {
-        "form": form
-    }
+            return HttpResponse('Error during category Edit')
     return render(request, "pages/category/category_new.html", data)        
 
 def category_detail(request, category_id):
     try:
-        category = Category.objects.get(id=category_id)
+        category = get_category(category_id)
     except:
         return HttpResponse(f"category: {category_id} doesnt exist")
     data = {
@@ -38,24 +50,81 @@ def category_detail(request, category_id):
     }
     return render(request, "pages/category/category_detail.html", data)
 
-def post_list(request, category_id):
-     try:
-        category = Category.objects.get(id=category_id)
-     except:
-        return HttpResponse(f"post: {category_id} doesnt exist")
+def category_delete(request, category_id):
+    category = get_category(category_id)
+    try:
+        category.delete()
+    except:
+        return HttpResponse(f"category: {category_id} doesnt exist")
+    return redirect("home")
 
+def post_new(request, category_id):
+     category = get_category(category_id)
+     form = PostForm(request.POST or None)
      data = {
-         "category": category
+         "add_or_edit": 'Add',
+         "category": category,
+         "form": form
      }
-     return render(request, "pages/post/post_list.html", data)
+     if request.method == "POST":
+        try: 
+            form.save()
+            return redirect("category_detail", category.id)
+        except:
+            return HttpResponse('Error during post creation')
+     return render(request, "pages/post/post_new.html", data)
+
+def post_list(request, category_id):
+    category = get_category(category_id)
+    data = {
+        "category": category
+    }
+    return render(request, "pages/post/post_list.html", data)
 
 def post_detail(request, category_id, post_id):
-    try:
-        post = Post.objects.get(id=post_id)
-    except:
-        return HttpResponse(f"post {post_id} doenst exist")
+    post = get_post(post_id)
+    category = get_category(category_id)
     data = {
-        "post": post
+        "post": post,
+        "category": category
     }
-    return render(request, "pages/post/post_detail.html", data)
+    return render(request, "pages/post/post_detail.html", data)    
 
+def post_edit(request, category_id, post_id):
+    category = get_category(category_id)
+    post = get_post(post_id)
+    form = PostForm(request.POST or None, instance=post)
+    data = {
+        "add_or_edit": "Edit",
+        "post": post,
+        "category": category,
+        "form": form
+    }
+    if request.method == "POST":
+        try:
+            form.save()
+            return redirect('post_list', category.id)
+        except:
+            HttpResponse(f"form doens't exist")
+    return render(request, "pages/post/post_new.html", data)
+
+def post_delete(request, category_id, post_id):
+    category = get_category(category_id)
+    post = get_post(post_id)
+    try:
+        post.delete()
+    except:
+        HttpResponse('Failed to Delete Post')
+    
+    return redirect('post_list', category.id)
+
+
+
+def get_category(category_id):
+    return Category.objects.get(id=category_id)
+def get_post(post_id):
+    return Post.objects.get(id=post_id)
+def get_all_categories():
+    return Category.objects.all()
+def get_all_posts():
+    return Post.objects.all()
